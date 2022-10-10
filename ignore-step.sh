ff2fc3bb70ddcba7a9a75e2848746078ef646a89
Estevão Vasques <estevaovasques@hotmail.com>
2022-10-10 15:24:13 -0300

    Alterado nome script e verificado branch para preview deploy

--- ignore-step.sh
+++ ignore-step.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+VERCEL_ENV="production"
 
 echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_ENV" == "production" ] ||  [ "$VERCEL_ENV" == "preview" ] ] ; then
+if [[ $VERCEL_ENV = 'production' || $VERCEL_GIT_COMMIT_REF = 'release' ]]; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
